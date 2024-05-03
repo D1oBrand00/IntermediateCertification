fdbc2615a38534a594894b462dcb8de193fa5860
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-03 13:23:00 +0300

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны A прямоугольника: ");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны B прямоугольника: ");
+            double side2 = double.Parse(Console.ReadLine());
+            Rectangle rect = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площадь прямоугольника: {rect.AreaCalculator()}\nПериметр прямоугольника: {rect.PerimeterCalculator()}");
+            Console.ReadKey();
+        }
+    }
+}
