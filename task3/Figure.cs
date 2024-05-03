bc2edd7b76b37ff046888e521cd4b8c1cc5224c4
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-03 14:12:11 +0300

    Update Figure.cs

--- task3/Figure.cs
+++ task3/Figure.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,35 @@ namespace task3
 {
     class Figure
     {
-        Point[] points;
-        public Figure(params Point[] points)
+        Point[] ps;
+        public Figure(Point[] ps)
         {
-            this.points = points;
+            this.ps = ps;
+
         }
-        public double LengthSide(Point A, Point B)
+        public double LengthSide(Point p1, Point p2)
         {
-            return Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+            return Math.Sqrt(Math.Pow((p1.X - p2.X), 2) + Math.Pow((p1.Y - p2.Y), 2));
         }
-        public void PerimeterCalculator()
+        public double PerimeterCalculator()
         {
-            
-            
+            double perimeter = 0;
+            for (int i = 0; i < ps.Length - 1; i++)
+            {
+                perimeter += LengthSide(ps[i], ps[i + 1]);
+            }
+            perimeter += LengthSide(ps[0], ps[ps.Length - 1]);
+            return perimeter;
+
+        }
+        public void Output()
+        {
+            if (ps.Length == 3)
+            { Console.Write($"Геом.фигура: треугольник\nПериметр фигуры: {PerimeterCalculator():F2}"); }
+            else if (ps.Length == 4)
+            { Console.Write($"Геом.фигура: прямоуголник\nПериметр фигуры: {PerimeterCalculator():F2}"); }
+            else if (ps.Length == 5)
+            { Console.Write($"Геом.фигура: пятиугольник\nПериметр фигуры: {PerimeterCalculator():F2}"); }
         }
     }
 }
