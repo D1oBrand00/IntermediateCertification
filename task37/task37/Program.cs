c8f775f084c2afbe2b85adcdf66c24a5ad2899cc
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-24 12:33:45 +0300

    Add files via upload

---
+++ task37/task37/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task37
+{
+    class Program
+    {
+        /// <summary>
+        /// Объявление делегата
+        /// </summary>
+        delegate double Square(double a, double b, double c);
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Создание объекта делегата
+            /// </summary>
+            Square del;
+            /// <summary>
+            /// Присвоение объекту делегата указателя на метод
+            /// </summary>
+            del = SquareTriangle.S;
+            Console.Write("Введите сторону A треугольника ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите сторону B треугольника ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите сторону C треугольника ");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write($"{del(a, b, c):F2}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ task37/task37/SquareTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task37
+{
+    static class SquareTriangle
+    {
+        public static double S(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                return Math.Sqrt(p * (p - a) * p * (p - b) * p * (p - c));
+            }
+            else { return 0; }
+        }
+    }
+}
