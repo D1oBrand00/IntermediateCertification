f4f602902edd2ec98f484f85cd720e494784ea38
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-24 12:41:22 +0300

    Add files via upload

---
+++ task44/task44/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task44
+{
+    /// <summary>
+    /// Объявление делегата
+    /// </summary>
+    /// <param name="m">начальное число m</param>
+    /// <param name="n">конечное число n</param>
+    /// <returns></returns>
+    delegate int Sum(int m, int n);
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Создание объекта делегата и присвоение ему указателя на статический метод Sum
+            /// </summary>
+            Sum del = Summm.Sum;
+            Console.Write("Ввелите начало интервала: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Ввелите конец интервала: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write(del(m, n));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task44/task44/Summm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task44
+{
+    static class Summm
+    {
+        public static int Sum(int m, int n)
+        {
+            int sum = 0;
+            for (int i = m;i<=n;i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
