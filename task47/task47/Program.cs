045c149f2a59a0d180d6541f3ef4b89245a3b9b0
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-24 12:38:03 +0300

    Add files via upload

---
+++ task47/task47/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Zakaz> zk = new List<Zakaz>();
+            while(true)
+            {
+                Console.Write("Добавить объект класса - 1\nВывести объект клаасса - 2\nВыйти из консоли - Escape");
+                char action = Console.ReadKey().KeyChar;
+                switch (action)
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.Write("Введите фамилию: ");
+                        string fam = Console.ReadLine();
+                        Console.Write("Введите кол-во мест : ");
+                        int size = int.Parse(Console.ReadLine());
+                        Console.Write("Введите комфорт: ");
+                        string comfort = Console.ReadLine();
+                        zk.Add(new Zakaz(fam, size, comfort));
+                        Console.Clear();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        foreach(var elem in zk)
+                        {
+                            elem.Show();
+                        }
+                        break;
+                    case '\x001B':
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.Write($"Неверно введено\n\n");
+                        break;
+
+                }    
+                
+
+            }    
+        }
+    }
+}
---
+++ task47/task47/Zakaz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task47
+{
+    class Zakaz
+    {
+        string fam;
+        int size;
+        string comfort;
+        public string Fam { get { return fam; } set { fam = value; } }
+        public int Size{ get { return size; } set { size = value; } }
+        public string Comfort{ get { return comfort; } set { comfort = value; } }
+        public Zakaz() { }
+        public Zakaz(string fam)
+        {
+            this.fam = fam;
+        }
+        public Zakaz(string fam,int size):
+            this(fam)
+        {
+            this.size = size;
+        }
+        public Zakaz(string fam, int size,string comfort):
+            this(fam,size)
+        {
+            this.comfort = comfort;
+        }
+        public void Show()
+        {
+            Console.Write($"Фамилия: {fam}\nКол-во мест в номере: {size}\nКомфорт: {comfort}\n\n");
+        }
+    }
+}
