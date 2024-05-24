dc5ba7fbfdc6946fd02f80a0450acd32389fadeb
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-24 12:42:22 +0300

    Add files via upload

---
+++ task40/task40/AvergaeValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task40
+{
+    static class AvergaeValue
+    {
+        public static double Avr(double m, double n)
+        {
+            double sum = 0;
+            double dif = n - m + 1;
+            for (double i = m; i <= n; i++)
+            {
+                sum += i;
+            }
+            return sum / dif; ;
+        }
+    }
+}
---
+++ task40/task40/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task40
+{
+    class Program
+    {
+        delegate double Avr(double m, double n);
+        static void Main(string[] args)
+        {
+            Avr del = AvergaeValue.Avr;
+            Console.Write("Введите начало интервала: ");
+            double  m = double.Parse(Console.ReadLine());
+            Console.Write("Введите конец интервала: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.Write(del(m, n));
+            Console.ReadKey();
+        }
+    }
+}
